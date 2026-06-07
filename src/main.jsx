4c9febf5cf72d1a0f9bb1ad8e0c0e570b36c2098
ksmith863 <kerry@gothamwoodworks.com>
2026-06-07 15:12:05 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './project-manager.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
